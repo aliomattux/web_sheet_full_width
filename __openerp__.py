742c0f2bf066eb6625008d4645b354f84ad5fbff
root <root@odoo.htm-mbs.com>
2015-12-02 23:49:46 -0500

    Commit OCA module

---
+++ __openerp__.py
@@ -0,0 +1,33 @@
+{
+    'name': 'Mage2Odoo',
+    'version': '1.1',
+    'author': 'Kyle Waid',
+    'category': 'Sales Management',
+    'depends': ['payment_method', 'delivery', 'stock_package', 'product_sku_upc', 'sale_stock'],
+    'website': 'https://www.gcotech.com',
+    'description': """ 
+    """,
+    'data': ['views/core.xml',
+	     'views/attribute.xml',
+	     'views/sites.xml',
+	     'views/mapping.xml',
+	     'views/jobs.xml',
+	     'views/product.xml',
+	     'views/import_export.xml',
+	     'views/product_category.xml',
+	     'views/model.xml',
+	     'views/misc.xml',
+	     'views/sale.xml',
+	     'views/stock.xml',
+	     'views/delivery.xml',
+	     'views/partner.xml',
+	     'data/mage_shipping.xml',
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'external_dependencies': {
+	'python': ['magento', 'pycountry'],
+    },
+}
